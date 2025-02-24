9d1b27abc9dac40463fe8486eaa1113cde774792
Winner Kelechi <nnaemeka.k@rytegate.com>
2025-02-24 10:29:55 +0100

    Temp Portfolio

--- resources/views/vendor/mail/html/header.blade.php
+++
@@ -1,7 +0,0 @@
-<tr>
-<td class="header">
-<a href="{{ $url }}" style="display: inline-block;">
-    <img src="{{ asset('images/stride-logo.png') }}" class="logo" alt="STRIDE ERP Logo">
-</a>
-</td>
-</tr>
